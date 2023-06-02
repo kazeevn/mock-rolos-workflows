c632fd28977d6b26f00107a65da3362d0290f186
Nikita Kazeev <kazeevn@gmail.com>
2023-06-02 23:19:34 +0800

    Removed host hardcoding, reduced nodes to 1 CPU and 2 Gb RAM

--- mock_project.js
+++ mock_project.js
@@ -9,8 +9,9 @@ function mock_current_project() {
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let project_url = new URL(tabs[0].url);
         const project_id = project_url.pathname.split("/")[2];
+        const api_head = project_url.host;
         console.log(`Project ${project_id}`);
-        mock_project(project_id).then(() => {
+        mock_project(project_id, api_head).then(() => {
             alert("Project mocked!");
             // document.getElementById('content').innerHTML = "<img src='images/complete.png' alt='Robot jester having fun' width='256px' height='256px'>"; 
             // chrome.windows.create({'url': 'complete.html', 'type': 'popup'})
@@ -20,8 +21,8 @@ function mock_current_project() {
     // const project_url = new URL(window.location.href);
 }
 
-async function mock_project(project_id) {
-    const workflows_response = await fetch(`https://my.rolos.com/api/v1/projects/${project_id}/workflows`, {
+async function mock_project(project_id, api_head) {
+    const workflows_response = await fetch(`https://${api_head}/api/v1/projects/${project_id}/workflows`, {
     "headers": {
         "accept": "*/*",
         "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
@@ -39,13 +40,13 @@ async function mock_project(project_id) {
     });
     const workflows = await workflows_response.json();
     for (const i in workflows['results']) {
-        await mock_workflow(workflows['results'][i]['id']);
+        await mock_workflow(workflows['results'][i]['id'], api_head);
     };
 }
 
-async function mock_workflow(workflow_id) {
+async function mock_workflow(workflow_id, api_head) {
     console.log(`Workflow ${workflow_id}`);
-    const nodes_response = await fetch(`https://my.rolos.com/api/v1/workflows/${workflow_id}/nodes`, {
+    const nodes_response = await fetch(`https://${api_head}/api/v1/workflows/${workflow_id}/nodes`, {
         "headers": {
           "accept": "*/*",
           "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
@@ -63,17 +64,17 @@ async function mock_workflow(workflow_id) {
       });
     const nodes = await nodes_response.json();
     for (const i in nodes['results']) {
-        await mock_node(nodes['results'][i]['id']);
+        await mock_node(nodes['results'][i]['id'], api_head);
     };
 }
 
-async function mock_node(node_id) {
-    body = {"cpu_count":2, "gpu":false, "ram":3, "type": "environment"};
+async function mock_node(node_id, api_head) {
+    body = {"cpu_count":1, "gpu":false, "ram":2, "type": "environment"};
     const c = document.getElementById("script_overwrite");
     if (c.checked) {
         body['file_id'] = "/mock.sh";
     };
-    fetch(`https://my.rolos.com/api/v1/workflows/nodes/${node_id}`, {
+    fetch(`https://${api_head}/api/v1/workflows/nodes/${node_id}`, {
         "headers": {
           "accept": "*/*",
           "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
